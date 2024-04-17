664e596933918e76fd56d6bd381e3a1128f176c1
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-04-17 17:04:01 -0500

    se crea examen

---
+++ src/database/usuarios.js
@@ -0,0 +1,19 @@
+export let usuarios = [
+    {
+      nombre: "tomas martinez",
+      documento: "1001234067",
+      telefono: "3045516490",
+      direccion: "carrera 74aa #1 a sur-210",
+      correo: "tillo.03044@gmail.com",
+      nota: 4,
+    },
+    {
+      nombre: "monica",
+      documento: "123456789",
+      telefono: "234567",
+      direccion: "a la vuelta de la esquina",
+      correo: "villana@gmail.com",
+      nota: "la propia villana",
+    }
+    
+  ];
